2064d13e30e463e0d1ba614f89705fb39cf67349
Wisdom <wisetega007@gmail.com>
2024-06-17 00:48:50 +0100

    chane-something

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -23,7 +23,9 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
       </h3>
     ) : (
       <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
-        <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}>
+        <div
+          className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
+        >
           <img
             src="/headset.svg"
             alt="headset"
